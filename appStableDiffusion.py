15ce8eb6c0a316e279c43d0398616b5ee3480119
lukexyz <lukedwoods@gmail.com>
2023-11-17 21:27:52 +0000

    remove data folders

--- appStableDiffusion.py
+++ appStableDiffusion.py
@@ -1,41 +1,57 @@
 import gradio as gr
-from PIL import Image, ImageDraw, ImageFont
-import random
 
-def generate_image(prompt, sampling_method, sampling_steps, width, height):
-    # Generate random RGB values for a shade of pink or purple
-    R = random.randint(200, 255)
-    G = random.randint(100, 192)
-    B = random.randint(180, 255)
-    img = Image.new('RGB', (width, height), (R, G, B))
-    draw = ImageDraw.Draw(img)
-    
-    # Set font size and type (use a default PIL font as an example; you can specify a different TTF font)
-    font_size = 40
-    try: font = ImageFont.truetype("arial.ttf", font_size)
-    except IOError: font = ImageFont.load_default()
-    
-    # Add text to image
-    text_color = (255 - R, 255 - G, 255 - B)  # Use complementary color for visibility
-    draw.text((10, 10), prompt, font=font, fill=text_color)
-    
-    return img
+# Dropdown for the checkpoint
+checkpoint_dropdown = gr.inputs.Dropdown(["protogenX340ficial@..."], label="Stable Diffusion checkpoint")
 
-with gr.Blocks() as demo:
-    gr.Markdown("Image Generation with Gradio")
-    prompt = gr.Textbox(lines=3, label="Your Prompt")
-    with gr.Row():
-        with gr.Column():
-            negative_prompt = gr.Textbox(lines=3, label="Negative Prompt")
-            steps = gr.Slider(label='Inference Steps',value=25,
-                              info="How many denoising steps?")
-            guidance = gr.Slider(label='Guidance Scale', minimum=1, maximum=20, value=7,
-                              info="Controls how much the text prompt influences the result.")
-            width = gr.Slider(label='Width', minimum=64, maximum=512, step=64, value=512)
-            height = gr.Slider(label='Height', minimum=64, maximum=512, step=64, value=512)
-            btn = gr.Button("Submit")
-        with gr.Column():
-            output = gr.Image(label='Result')
+# Textbox for the negative prompt
+negative_prompt = gr.inputs.Textbox(lines=3, placeholder="Negative prompt")
 
-        btn.click(fn=generate_image, inputs=[prompt, steps, guidance, width, height], outputs=[output])
-    demo.launch(share=False)
+# Dropdown for the sampling method
+sampling_method = gr.inputs.Dropdown(["Euler a"], label="Sampling method")
+
+# Slider for the sampling steps
+sampling_steps = gr.inputs.Slider(minimum=1, maximum=100, default=20, label="Sampling steps")
+
+# Dropdown for the script selection
+script = gr.inputs.Dropdown(["None"], label="Script")
+
+# Checkboxes
+checkboxes = gr.inputs.Radio(
+    options=["Restore faces", "Tiling", "Hires. fix"], 
+    type="checkbox",
+    label="Options"
+)
+
+# Numeric Input fields for Width, Height, Batch count, Batch size
+width = gr.inputs.Number(default=512, label="Width")
+height = gr.inputs.Number(default=512, label="Height")
+batch_count = gr.inputs.Number(default=4, label="Batch count")
+batch_size = gr.inputs.Number(default=1, label="Batch size")
+
+# Slider for CFG Scale
+cfg_scale = gr.inputs.Slider(minimum=1, maximum=100, default=12, label="CFG Scale")
+
+# Numeric Input for Seed
+seed = gr.inputs.Number(default=144178169, label="Seed")
+
+# Image output and style selectors
+image_output = gr.outputs.Image()
+style_1 = gr.inputs.Dropdown(["None"], label="Style 1")
+style_2 = gr.inputs.Dropdown(["None"], label="Style 2")
+
+# Dummy function to pass into Interface
+def dummy_function(*args):
+    return "https://via.placeholder.com/512"
+
+# Create the Gradio Interface
+interface = gr.Interface(
+    fn=dummy_function,
+    inputs=[
+        checkpoint_dropdown, negative_prompt, sampling_method, sampling_steps, script,
+        checkboxes, width, height, batch_count, batch_size, cfg_scale, seed, style_1, style_2
+    ],
+    outputs=image_output,
+    live=True
+)
+
+interface.launch()
